8f5c19195d6d1ce84e7593f031abfb1f0fe3c96a
Praphulkailkhura <141400257+Praphulkailkhura@users.noreply.github.com>
2025-03-02 22:05:14 +0530

    Bottom Navigation

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,20 +2,44 @@ import { Tabs } from "expo-router/tabs";
 import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default function RootLayout() {
-  return ( <Tabs>
+  return ( <Tabs
+  screenOptions={{
+    tabBarActiveTintColor : "#1A34BD",
+    tabBarInactiveTintColor : "gray",
+    tabBarStyle : {backgroundColor: "white"},
+  }}
+  >
+
     <Tabs.Screen name="index" options={{ title: "Home",
-      tabBarIcon : ({color,size}) => (
-        <Icon name = "Home" size={size} color = "red"/>
+      tabBarIcon : ({color, size}) => (
+        <Icon name = "home" size={size} color = {color}/>
       )
     }} />
 
-    <Tabs.Screen name="bookings" options={{ title: "Bookings" }} />
+    <Tabs.Screen name="bookings" options={{ title: "Bookings",
+      tabBarIcon : ({color, size}) => (
+        <Icon name = "calendar-today" size={size} color = {color}/>
+      )
+     }} />
 
-    <Tabs.Screen name="scanqr" options={{ title: "Scan QR" }} />
+    <Tabs.Screen name="scanqr" options={{ title: "Scan QR",
+      tabBarIcon : ({color, size}) => (
+        <Icon name = "qr-code-scanner" size={size} color = {color}/> 
+      )
+    }} />
 
-    <Tabs.Screen name="wallet" options={{ title: "Wallet" }} />
+    <Tabs.Screen name="wallet" options={{ title: "Wallet",
+      tabBarIcon : ({color, size}) => (
+        <Icon name = "account-balance-wallet" size={size} color = {color}/> 
+   )
+    }} />
     
-    
+    <Tabs.Screen name="profile" options={{ title: "Profile",
+      tabBarIcon : ({color, size}) => (
+        <Icon name = "account-circle" size={size} color = {color}/> 
+      )
+  }} />
+
   </Tabs>
   )
 }
